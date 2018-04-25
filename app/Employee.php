6f6aa2967533559d15e87cdb8c1bc0288f8b9395
deeem <karpov.dimon@gmail.com>
2018-04-25 21:57:29 +0300

    mutate hired date

--- app/Employee.php
+++ app/Employee.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
+    /**
+     * The attributes that should be mutated to dates.
+     */
+    protected $dates = [
+        'hired',
+    ];
+
     public function superviser()
     {
         return $this->belongsTo(self::class, 'parent_id', 'id');
--- resources/views/partials/employee-row.blade.php
+++ resources/views/partials/employee-row.blade.php
@@ -1,6 +1,6 @@
 <tr>
   <td>{{ $employee->name }}</td>
   <td>{{ $employee->position }}</td>
-  <td>{{ $employee->hired }}</td>
+  <td>{{ $employee->hired->toFormattedDateString() }}</td>
   <td>{{ $employee->salary }}</td>
 </tr>
