e3823b11c822d652dee491e60848e58d293cc6f8
deeem <karpov.dimon@gmail.com>
2018-04-28 23:08:36 +0300

    display chief name

--- resources/views/employee/show.blade.php
+++ resources/views/employee/show.blade.php
@@ -8,6 +8,7 @@
     <p><span class="font-weight-bold">Position</span> {{ $employee->position }}</p>
     <p><span class="font-weight-bold">Hired date</span> {{ $employee->hired->toFormattedDateString() }}</p>
     <p><span class="font-weight-bold">Salary</span> {{ $employee->salary }}</p>
+    <p><span class="font-weight-bold">Chief</span> {{ $employee->superviser->name }}</p>
   </div>
 </div>
 <div class="row justify-content-center">
