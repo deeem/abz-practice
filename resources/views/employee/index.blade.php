dbee9c64076ffc35e3371f389951a3683d8de23a
deeem <karpov.dimon@gmail.com>
2018-04-28 12:05:58 +0300

    add employee button

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -2,12 +2,17 @@
 
 @section('content')
 <div class="row justify-content-center">
-  <p class="employee-table-sort">
-    <a href="{{ route('employee.index', ['sort' => 'name']) }}">Sort by name</a>&nbsp;&nbsp;&nbsp;
-    <a href="{{ route('employee.index', ['sort' => 'position']) }}">Sort by position</a>&nbsp;&nbsp;&nbsp;
-    <a href="{{ route('employee.index', ['sort' => 'hired']) }}">Sort by hired</a>&nbsp;&nbsp;&nbsp;
-    <a href="{{ route('employee.index', ['sort' => 'salary']) }}">Sort by salary</a>
-  </p>
+  <div class="col-md3 ml-md-auto">
+    <p class="employee-table-sort">
+      <a href="{{ route('employee.index', ['sort' => 'name']) }}">Sort by name</a>&nbsp;&nbsp;&nbsp;
+      <a href="{{ route('employee.index', ['sort' => 'position']) }}">Sort by position</a>&nbsp;&nbsp;&nbsp;
+      <a href="{{ route('employee.index', ['sort' => 'hired']) }}">Sort by hired</a>&nbsp;&nbsp;&nbsp;
+      <a href="{{ route('employee.index', ['sort' => 'salary']) }}">Sort by salary</a>
+    </p>
+  </div>
+  <div class="col-md-3 ml-md-auto">
+    <a href="{{ route('employee.create') }}" class="btn btn-success">Add employee</a>
+  </div>
 </div>
 
 <div class="row justify-content-center employee-table-search">
