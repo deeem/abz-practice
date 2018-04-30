a0ae862cc0e2c382f0d3f7947eec50b12bc8023c
deeem <karpov.dimon@gmail.com>
2018-04-30 15:20:13 +0300

    thumbs in employee tree

--- resources/views/partials/employee.blade.php
+++ resources/views/partials/employee.blade.php
@@ -1,5 +1,14 @@
 <div class="employee-list-item bg-white rounded shadow">
-  <p>{{ $employee->name }}, <span class="text-secondary">{{ $employee->position }}</span></p>
+  <div class="row">
+    <div class="col-3">
+      @if($employee->photo)
+        <img class="img-thumbnail" src="{{ asset('storage/thumbs/' . $employee->photo) }}">
+      @endif
+    </div>
+    <div class="col-9">
+      <p>{{ $employee->name }}, <span class="text-secondary">{{ $employee->position }}</span></p>
+    </div>
+  </div>
 </div>
 @if($employee->subordinates->count())
   <div class="employee-list pl-5">
